b0ff184ae817e5cc44e55167c470f5b4fed8bc1e
Zhiyi Zhang <zzhang867@wisc.edu>
2019-05-02 15:57:20 -0500

    add topic

--- application/QuizGenerator.java
+++ application/QuizGenerator.java
@@ -167,6 +167,7 @@ public class QuizGenerator implements GeneratorInterface {
   @Override
   public void addNewQuestion(Question newQuestion) {
     questionBank.add(newQuestion);
+    topicList.add(newQuestion.topic);
   }
 
   /**
