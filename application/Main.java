a8e55d71c1ece860dbdb276ac6632c8eb5b75be1
zhiyi <zhiyi@10.140.159.213>
2019-05-03 16:59:31 -0500

    Split class

--- application/Main.java
+++ application/Main.java
@@ -44,18 +44,18 @@ import org.json.simple.parser.ParseException;
  * @author Authors: Aaron Zhang, Aurora Shen, Tyler Gu, Yixing Tu
  */
 public class Main extends Application {
-  QuizGenerator quizGenerator = new QuizGenerator(); // Instance of generator
-  boolean needQuit = false; // boolean variable to detect if user wants to quit
-  Stage stage; // Primary stage
-  private HashMap<String, BorderPane> screenMap = new HashMap<>(); // Map stores all screens
-  private Scene main; // Scene to display different panes
-  BorderPane root; // the main menu
-  Insets insets = new Insets(10); // Insets used for formatting
-  String inputFileName = null; // File we read from
-  int count; // Count how many questions have been answered
-  List<String> filesOpened = new ArrayList<>(); // Record files have been read already
-  boolean saved = true; // boolean variable to detect if all changes have been saved properly
-  ToggleGroup answergroup;
+  protected QuizGenerator quizGenerator = new QuizGenerator(); // Instance of generator
+  protected boolean needQuit = false; // boolean variable to detect if user wants to quit
+  protected Stage stage; // Primary stage
+  protected HashMap<String, BorderPane> screenMap = new HashMap<>(); // Map stores all screens
+  protected Scene main; // Scene to display different panes
+  protected BorderPane root; // the main menu
+  protected Insets insets = new Insets(10); // Insets used for formatting
+  protected String inputFileName = null; // File we read from
+  protected int count; // Count how many questions have been answered
+  protected List<String> filesOpened = new ArrayList<>(); // Record files have been read already
+  protected boolean saved = true; // boolean variable to detect if all changes have been saved properly
+  protected ToggleGroup answergroup;
 
   /**
    * Driver
@@ -188,6 +188,39 @@ public class Main extends Application {
   }
 
   /**
+   * Set up screens according to user input
+   * 
+   * @param name is the name of screen which user is calling
+   */
+  public void setupScreens(String name) {
+    switch (name) {
+      case "add":
+        AddQuestionScreen aqs = new AddQuestionScreen();
+        aqs.setUpAddScreen(screenMap.get(name));
+        break;
+      case "load1":
+        this.setUpLoad1Screen(screenMap.get(name));
+        break;
+      case "load2":
+        this.setUpLoad2Screen(screenMap.get(name));
+        break;
+      case "next":
+        this.setUpNextScreen(screenMap.get(name));
+        break;
+      case "save":
+        this.setUpSaveScreen(screenMap.get(name));
+        break;
+      case "exit":
+        this.setUpExitScreen(screenMap.get(name));
+        break;
+      case "beforeLoading":
+        this.setUpBeforeLoadingScreen(screenMap.get(name));
+        break;
+    }
+
+  }
+  
+  /**
    * Generate a border pane and add it into map
    * 
    * @param name
@@ -218,189 +251,6 @@ public class Main extends Application {
   public void generateQuiz(List<String> topic, int amount)
       throws FileNotFoundException, IOException, ParseException {
     quizGenerator.generateQuiz(topic, amount);
-  }
-
-  /**
-   * This method sets up the add screen when Add button is clicked
-   * 
-   * @param pane is the pane we are setting up
-   */
-  public void setUpAddScreen(BorderPane pane) {
-    saved = false; // update new change so that user will be asked to save
-    VBox vbox = new VBox();
-    TextField textField; // common textField reference to use in this method
-
-    // Set the text at the top
-    Text text = new Text("Add new question");
-    text.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
-    pane.setTop(text);
-
-    // initialize a HBox for text of the question
-    // and add to the vbox
-    HBox hbox = new HBox();
-    TextField question = new TextField(); // text field for question
-    question.setPromptText("Type in question");
-    hbox.getChildren().addAll(new Text("Text: "), question);
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER); // align to the center
-    // initialize a new HBox for topic of the question
-    hbox = new HBox();
-    TextField topic = new TextField(); // text field for topic
-    topic.setPromptText("Type in topic");
-    hbox.getChildren().addAll(new Text("Topic: "), topic);
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER);
-    // initialize a new HBox for Image file name of the question
-    hbox = new HBox();
-    TextField image = new TextField();
-    image.setPromptText("Type in image name"); // text field for image path
-    hbox.getChildren().addAll(new Text("Image: "), image);
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER);
-    // initialize a new HBox for texts
-    hbox = new HBox();
-    hbox.getChildren().add(new Text("Choices: "));
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER);
-
-    // toggle group of radio buttons so that only one selection can be chosen
-    ToggleGroup group = new ToggleGroup();
-    // five radio buttons corresponding to five text fields
-    RadioButton button1 = new RadioButton();
-    RadioButton button2 = new RadioButton();
-    RadioButton button3 = new RadioButton();
-    RadioButton button4 = new RadioButton();
-    RadioButton button5 = new RadioButton();
-    // add buttons into togglegroup
-    button1.setToggleGroup(group);
-    button2.setToggleGroup(group);
-    button3.setToggleGroup(group);
-    button4.setToggleGroup(group);
-    button5.setToggleGroup(group);
-    button1.setSelected(true);
-
-    // set up the text fields for user input
-    TextField choice1 = new TextField();
-    TextField choice2 = new TextField();
-    TextField choice3 = new TextField();
-    TextField choice4 = new TextField();
-    TextField choice5 = new TextField();
-    choice1.setPromptText("Choice 1");
-    hbox = new HBox();
-    hbox.getChildren().setAll(button1, choice1);
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER);
-    choice2.setPromptText("Choice 2");
-    hbox = new HBox();
-    hbox.getChildren().setAll(button2, choice2);
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER);
-    choice3.setPromptText("Choice 3");
-    hbox = new HBox();
-    hbox.getChildren().setAll(button3, choice3);
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER);
-    choice4.setPromptText("Choice 4");
-    hbox = new HBox();
-    hbox.getChildren().setAll(button4, choice4);
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER);
-    choice5.setPromptText("Choice 5");
-    hbox = new HBox();
-    hbox.getChildren().setAll(button5, choice5);
-    vbox.getChildren().add(hbox);
-    hbox.setAlignment(Pos.CENTER);
-    vbox.setSpacing(10);
-
-    // set up the buttons and their functionality
-    Button saveButton = new Button("Add");
-    Button cancelButton = new Button("Cancel");
-    hbox = new HBox();
-    hbox.getChildren().addAll(saveButton, cancelButton);
-    hbox.setSpacing(10);
-    hbox.setAlignment(Pos.CENTER_RIGHT);
-
-    // when save button is clicked, check if any required field is empty
-    // create a new question and save to question bank
-    saveButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-      @Override
-      public void handle(MouseEvent me) {
-        main.setRoot(root);
-        String[] choiceArray = new String[5];
-        String answer;
-        choiceArray[0] = choice1.getText();
-        choiceArray[1] = choice2.getText();
-        choiceArray[2] = choice3.getText();
-        choiceArray[3] = choice4.getText();
-        choiceArray[4] = choice5.getText();
-
-        // if question text field or topic field is empty
-        // show alert
-        if (question.getText().isEmpty() || topic.getText().isEmpty()) {
-          Alert alert = new Alert(AlertType.INFORMATION);
-          alert.setTitle("Alert");
-          alert.setHeaderText("Empty Text Field!");
-          alert.setContentText("Question Text can not be empty");
-          activate("add");
-          setupScreens("add");
-          alert.showAndWait();
-          return;
-        }
-        // image field is not required, if empty, set to "none"
-        if (image.getText().isEmpty()) {
-          image.setText("none");
-        }
-
-        // find the correct choice
-        TextField rightTF;
-        if (group.getSelectedToggle().equals(button1)) {
-          rightTF = choice1;
-        } else if (group.getSelectedToggle().equals(button2)) {
-          rightTF = choice2;
-        } else if (group.getSelectedToggle().equals(button3)) {
-          rightTF = choice3;
-        } else if (group.getSelectedToggle().equals(button4)) {
-          rightTF = choice4;
-        } else {
-          rightTF = choice5;
-        }
-
-        // if correct choice is empty, display alert
-        // else, set the correct answer
-        if (rightTF.getText().isEmpty()) {
-          Alert alert = new Alert(AlertType.INFORMATION);
-          alert.setTitle("Alert");
-          alert.setHeaderText("Empty Right Choice Selected!");
-          alert.setContentText("Empty choice cannot be selected as correct");
-          activate("add");
-          setupScreens("add");
-          alert.showAndWait();
-          return;
-        } else {
-          answer = rightTF.getText();
-        }
-
-        Question newQuestion =
-            new Question(question.getText(), choiceArray, image.getText(), topic.getText(), answer);
-        quizGenerator.addNewQuestion(newQuestion);
-      }
-    });
-
-    // if cancel button is clicked, go back to root
-    cancelButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-      @Override
-      public void handle(MouseEvent me) {
-        main.setRoot(root);
-      }
-    });
-
-    pane.setBottom(hbox);
-    pane.setAlignment(saveButton, Pos.CENTER_RIGHT);
-    pane.setCenter(vbox);
-    pane.setAlignment(vbox, Pos.CENTER);
-    pane.setMargin(pane.getTop(), insets);
-    pane.setMargin(pane.getCenter(), insets);
-    pane.setMargin(pane.getBottom(), insets);
   }
 
   /**
@@ -898,38 +748,6 @@ public class Main extends Application {
     pane.setCenter(vbox);
     pane.setMargin(pane.getTop(), insets);
     pane.setMargin(pane.getCenter(), insets);
-  }
-
-  /**
-   * Set up screens according to user input
-   * 
-   * @param name is the name of screen which user is calling
-   */
-  public void setupScreens(String name) {
-    switch (name) {
-      case "add":
-        this.setUpAddScreen(screenMap.get(name));
-        break;
-      case "load1":
-        this.setUpLoad1Screen(screenMap.get(name));
-        break;
-      case "load2":
-        this.setUpLoad2Screen(screenMap.get(name));
-        break;
-      case "next":
-        this.setUpNextScreen(screenMap.get(name));
-        break;
-      case "save":
-        this.setUpSaveScreen(screenMap.get(name));
-        break;
-      case "exit":
-        this.setUpExitScreen(screenMap.get(name));
-        break;
-      case "beforeLoading":
-        this.setUpBeforeLoadingScreen(screenMap.get(name));
-        break;
-    }
-
   }
 
   /**
